ac892aefc4ca5f4fd83fa58b2a96c76bf5401d91
hwaner <ljhwan8111@naver.com>
2022-04-06 09:56:02 +0900

    re

--- mygarden/mygarden.sh
+++ mygarden/mygarden.sh
@@ -3,7 +3,7 @@
 cd /home/hwaner/Documents/shell/mygarden
 git add .
 git commit -m "crontab commit"
-git push origin master
+git push origin main
 exit 0
 2022. 04. 05. (화) 13:30:01 KST
 2022. 04. 05. (화) 14:00:01 KST
